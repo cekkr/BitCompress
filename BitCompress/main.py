7efaa35cecedb16220ebe2db74530cf25ef29268
Riccardo Cecchini <rcecchini.ds@gmail.com>
2024-10-07 14:11:44 +0200

    Update main.py

--- BitCompress/main.py
+++ BitCompress/main.py
@@ -388,6 +388,7 @@ class GateBranch:
         # Look for XOR
         # (A*B)+(!A*!B) => !XOR(A,B) => AND!(A,B)+AND()
         # (!A*B)+(A*!B) => XOR(A,B) => AND!(A)+AND(B)
+        # !EXOR(A,B,C) => (!A*!B*!C)+(A*B*C)
         # Simple and superficial implementation
         #args_in_ports = self.calculate_args_in_ports()
         #empty_gate = self.get_empty_gate() # recalcualtion not needed: it never disappears
